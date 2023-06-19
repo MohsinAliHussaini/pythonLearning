76c317d1156dcb89a90ac3473860ccff7de3aba0
Mohsin Ali Hussaini <mohsinstd322@gmail.com>
2023-06-20 03:23:37 +0500

    Variable Program

---
+++ variable.py
@@ -0,0 +1,38 @@
+# Variables And User Inputs 
+# Variable woh cheez hoti hai jo change hoti rahe For Example : Temp, Speed
+# Programming Variable aik box ki tarah jis main values rakhte hai , values replace, update
+#a = 10 
+#print("a=",a)
+#a = 20 
+#print("a=",a)
+#a= "Hello World"
+#print("a=",a)
+
+#a = True
+#print("a=",a)
+
+#color = "Red"
+#print("My Favorite color is",color)
+#fruit = "Apple" Static Data
+#print("My Favorite Fruit is",fruit)
+
+color = input("What's your favorite color? ") # Dynamic Data
+print("Your Favorite Color is",color)
+number = int(input("What is your favorite number? ")) # Dynamic Data
+print("Your Favorite Number is ",number)
+
+
+# Practice
+# Question 1: What is Your Name? 
+# Questino 2: What is Your Age? 
+# Question 3: What is Your Qualifiction? 
+# Questino 4: What is your School/ College/ University Name ? 
+
+# Answer 1: Your Name is name
+# Answer 2: Your Age is age
+# Answer 3: Your Qualification is qualification 
+# Answer 4: Your School/ College Name / University Name is schoolName
+
+
+
+
